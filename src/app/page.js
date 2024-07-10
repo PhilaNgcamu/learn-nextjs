3c8e20214fc11e6d19478c0047687d0ed48e74ce
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-10 16:35:56 +0200

    Link component navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <div>Welcome back!</div>;
+  return (
+    <>
+      <h1>Welcome home!</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">Products</Link>
+    </>
+  );
 };
 
 export default Home;
