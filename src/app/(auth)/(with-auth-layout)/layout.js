91c2c69f59c16c85b42b24cdf39d76d1204fd1b8
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-10 16:59:33 +0200

    Active Links

--- src/app/(auth)/(with-auth-layout)/layout.js
+++ src/app/(auth)/(with-auth-layout)/layout.js
@@ -1,16 +1,44 @@
+"use client";
 import { Inter } from "next/font/google";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import "./styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const navLinks = [
+  {
+    name: "Register",
+    href: "/register",
+  },
+  {
+    name: "Login",
+    href: "/login",
+  },
+  {
+    name: "Forgot Password",
+    href: "/forgot-password",
+  },
+];
+
 const AuthLayout = ({ children }) => {
+  const pathname = usePathname();
   return (
     <>
-      <div
-        className={
-          (inter.className, "text-3xl text-center text-green-500 font-bold")
-        }
-      >
-        <h2>Inner Layout</h2>
+      <div>
+        {navLinks.map((link) => {
+          const isActive = pathname.startsWith(link.href);
+
+          return (
+            <Link
+              href={link.href}
+              key={link.name}
+              className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
         {children}
       </div>
     </>
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,25 +1,24 @@
 import Link from "next/link";
 
+const navLink = [
+  {
+    product: "Product 1",
+    href: "products/1",
+  },
+  { product: "Product 2", href: "products/1" },
+  { product: "Product 3", href: "products/3" },
+];
+
 const ProductsList = () => {
-  const productId = 100;
   return (
     <>
       <Link href="/">Home</Link>
       <h1>Product List</h1>
-      <h2>
-        <Link href="products/1">Product 1</Link>
-      </h2>
-      <h2>
-        <Link href="products/2">Product 2</Link>
-      </h2>
-      <h2>
-        <Link href="products/3" replace>
-          Product 3
-        </Link>
-      </h2>
-      <h2>
-        <Link href={`products/${productId}`}>Product {productId}</Link>
-      </h2>
+      {navLink.map((link) => (
+        <h2>
+          <Link href={link.href}>{link.product}</Link>
+        </h2>
+      ))}
     </>
   );
 };
