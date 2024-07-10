b2068bcbafd9a74ab8c81d3c9952b9f7ec33af3d
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-10 14:34:45 +0200

    Not found

--- src/app/products/[productId]/reviews/[reviewsId]/page.js
+++ src/app/products/[productId]/reviews/[reviewsId]/page.js
@@ -1,4 +1,9 @@
+import { notFound } from "next/navigation";
+
 const ReviewDetail = ({ params }) => {
+  if (parseInt(params.reviewsId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Review {params.reviewsId} for product {params.productId}
