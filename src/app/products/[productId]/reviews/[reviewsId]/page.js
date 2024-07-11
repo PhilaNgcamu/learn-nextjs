da84fceb90dd016ad01cfccc12e421ae2191b004
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-11 08:32:56 +0200

    Error Handling

--- src/app/products/[productId]/reviews/[reviewsId]/page.js
+++ src/app/products/[productId]/reviews/[reviewsId]/page.js
@@ -1,8 +1,16 @@
 import { notFound } from "next/navigation";
 
+const getRandomInt = (count) => {
+  return Math.floor(Math.random() * count);
+};
+
 const ReviewDetail = ({ params }) => {
+  const random = getRandomInt(2);
   if (parseInt(params.reviewsId) > 1000) {
     notFound();
+  }
+  if (random === 1) {
+    throw new Error("Error loading review");
   }
   return (
     <h1>
