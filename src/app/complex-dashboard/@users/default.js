9de03b88dec963324f1c9c477c93fdb369b70a86
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-11 16:42:04 +0200

    Parallel Routes

---
+++ src/app/complex-dashboard/@users/default.js
@@ -0,0 +1,7 @@
+import Card from "@/components/card";
+
+const DefaultUsersAnalytics = () => {
+  return <Card>Users analytics</Card>;
+};
+
+export default DefaultUsersAnalytics;
---
+++ src/app/complex-dashboard/default.js
@@ -0,0 +1,7 @@
+import Card from "@/components/card";
+
+const DefaultComplexDashboardPage = () => {
+  return <Card>Users analytics</Card>;
+};
+
+export default DefaultComplexDashboardPage;
