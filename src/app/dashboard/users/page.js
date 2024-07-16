da336dcd2f5bd543b987d2d0c10f3e4004d80ef8
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-16 11:34:20 +0200

    Search

--- src/app/dashboard/users/page.js
+++ src/app/dashboard/users/page.js
@@ -1,5 +1,17 @@
-const UsersPage = () => {
-  return <div>Users</div>;
-};
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Link from "next/link";
 
-export default UsersPage;
+export default function UsersPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder={"Search for a user..."} />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}></table>
+    </div>
+  );
+}
