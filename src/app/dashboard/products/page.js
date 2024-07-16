727733db5aca0b3f18e1373846bd84e759f2008b
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-16 14:28:46 +0200

    Users and Products Page

--- src/app/dashboard/products/page.js
+++ src/app/dashboard/products/page.js
@@ -1,5 +1,67 @@
-const ProductsPage = () => {
-  return <div>Products</div>;
-};
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 
-export default ProductsPage;
+export default function ProductsPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder={"Search for a product..."} />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Email</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr key="1">
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>john.doe@gmail.com</td>
+            <td>2022-09-09</td>
+            <td>Client</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/dashboard/users/1">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <form>
+                  <input type="hidden" name="id" value="1" />
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </form>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
+}
--- src/app/dashboard/users/page.js
+++ src/app/dashboard/users/page.js
@@ -1,5 +1,7 @@
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 import Search from "@/app/ui/dashboard/search/search";
 import styles from "@/app/ui/dashboard/users/users.module.css";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function UsersPage() {
@@ -12,8 +14,53 @@ export default function UsersPage() {
         </Link>
       </div>
       <table className={styles.table}>
-        <thead></thead>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr key="1">
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>john.doe@gmail.com</td>
+            <td>2022-09-09</td>
+            <td>Client</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/dashboard/users/1">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <form>
+                  <input type="hidden" name="id" value="1" />
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </form>
+              </div>
+            </td>
+          </tr>
+        </tbody>
       </table>
+      <Pagination />
     </div>
   );
 }
